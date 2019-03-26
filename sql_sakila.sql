648327b470d1194c28f0cea388d3c3781a82eeeb
deottoni <deottoni@gmail.com>
2019-03-26 00:51:28 -0500

    improved code, fixed some styling

--- sql_sakila.sql
+++ sql_sakila.sql
@@ -127,9 +127,9 @@ SELECT COUNT(*)
 FROM inventory
 WHERE film_id IN 
 	(
-		SELECT film_id
-		FROM film
-		WHERE title = "Hunchback Impossible"
+	SELECT film_id
+	FROM film
+	WHERE title = "Hunchback Impossible"
     );
 
 
@@ -164,14 +164,14 @@ SELECT first_name, last_name
 FROM actor
 WHERE actor_id IN
 	(
-		SELECT actor_id
-        FROM film_actor
-        WHERE film_id IN
-			(
-				SELECT film_id
-                FROM film
-                WHERE title LIKE "Alone Trip"
-            )
+	SELECT actor_id
+	FROM film_actor
+	WHERE film_id IN
+		(
+		SELECT film_id
+		FROM film
+		WHERE title LIKE "Alone Trip"
+		)
     );
 
 
